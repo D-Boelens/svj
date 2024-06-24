e19f1054b63a4a74876fdaf4d54327fc35cd6d4d
D-Boelens <dboelens1988@gmail.com>
2024-06-24 14:47:02 +0200

    Premie jaarlijks v1

--- svj.js
+++ svj.js
@@ -1,44 +1,52 @@
-const kortingPerTrede = [20, 30, 40, 45, 50, 55, 60, 65, 67, 69, 70, 71, 72, 73, 74, 75, 75, 75, 75, 75]; //Kortingspercentages per bonus-malus trede.
-const svjPerTrede = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]; //Svj per bonus-malus trede.
+const kortingPerTrede = [20, 30, 40, 45, 50, 55, 60, 65, 67, 69, 70, 71, 72, 73, 74, 75, 75, 75, 75, 75, 75]; //Kortingspercentages per bonus-malus trede.
 const userInputSvj = document.getElementById('svj-field-input-id');
 const userInputSchade = document.getElementById('schades-field-input-id');
-const userInputPremie = document.getElementById('premie-field-input-id');
+const userInputWaPremie = document.getElementById('wa-premie-field-input-id');
+const userInputCascoPremie = document.getElementById('casco-premie-field-input-id');
 const buttonBevestigen = document.getElementById('button-bevestigen-id');
 const huidigeKorting = document.getElementById('korting-percentage-id');
-const uitkomst = document.getElementById('korting-id');// Nog gebruiken voor complerte innerHTML van uitkomst
+const uitkomst = document.getElementById('korting-id'); // Nog gebruiken voor complerte innerHTML van uitkomst
 const svjNieuw = document.getElementById('svj-nieuw-id');
 const kortingNieuw = document.getElementById('korting-nieuw-id');
+const premieNieuw = document.getElementById('premie-nieuw-id');
+
+//Nog inbouwen: Geen negatieve schades/Required fields overall maken/Alerts.
 
 function malusBerekening() {
 	let svjInput = parseInt(userInputSvj.value);
 	let schadesInput = parseInt(userInputSchade.value);
+	let waPremieInput = parseInt(userInputWaPremie.value);
 	let svjNaSchade;
 	let kortingTrede;
 	let kortingsTredeNaSchade;
 
+	if (isNaN(schadesInput, svjInput) || schadesInput < 0) {
+		alert('Voer s.v.p. een geldig aantal in');
+		return;
+	}
+
 	//Geeft huidige korting weer op basis van svjInput klant
-	if (svjInput >= -4 && svjInput < 14) {
+	if (svjInput >= -4 && svjInput < 15) {
 		kortingTrede = svjInput + 5;
-	} else if (svjInput >= 14) {
-		kortingTrede = 19;
+	} else if (svjInput >= 15) {
+		kortingTrede = 20;
 	} else if (svjInput <= -5) {
 		kortingTrede = 0;
 	}
-  huidigeKorting.innerHTML = kortingPerTrede[kortingTrede];
-  
+	huidigeKorting.innerHTML = kortingPerTrede[kortingTrede] + '%  korting (trede ' + (kortingTrede + 1) + ')';
+
 	//Aantal svj na schadeclaim(s)
-  if (svjInput >= 14 ) {
-    svjInput = 14
-  }  
-	if (schadesInput >= 1 && schadesInput <= 3) {
+	if (svjInput >= 14) {
+		svjInput = 14;
+	}
+	if (schadesInput >= 0 && schadesInput <= 3) {
 		svjNaSchade = svjInput - schadesInput * 5;
 	} else if (schadesInput >= 4) {
-		svjNaSchade = 0;
-	} 
-	// Houdt uitkomst svjNaSchade binnen array svjPerTrede
-	svjNaSchade = Math.max(0, Math.min(svjNaSchade, svjPerTrede.length - 1));
-  svjNieuw.innerHTML = svjPerTrede[svjNaSchade];
-  
+		svjNaSchade = -5;
+	}
+	svjNaSchade = Math.max(-5, Math.min(svjNaSchade, 14));
+	svjNieuw.innerHTML = svjNaSchade;
+
 	//Aantal procent korting na schadeclaim(s)
 	if (schadesInput >= 1 && schadesInput <= 3) {
 		kortingsTredeNaSchade = kortingTrede - schadesInput * 5;
@@ -47,9 +55,20 @@ function malusBerekening() {
 	} else {
 		kortingsTredeNaSchade = kortingTrede;
 	}
-	// Houdt uitkomst kortingsTredeNaSchade binnen array kortingPerTrede
 	kortingsTredeNaSchade = Math.max(0, Math.min(kortingsTredeNaSchade, kortingPerTrede.length - 1));
-	kortingNieuw.innerHTML = kortingPerTrede[kortingsTredeNaSchade];
+	kortingNieuw.innerHTML = kortingPerTrede[kortingsTredeNaSchade] + '%  korting (trede ' + (kortingsTredeNaSchade + 1) + ')';
+
+	//Nieuwe premie na schadeclaim(s)
+	if (!isNaN(waPremieInput)) {
+		let huidigeKortingPercentage = kortingPerTrede[kortingTrede];
+		let nieuweKortingPercentage = kortingPerTrede[kortingsTredeNaSchade];
+		let oorspronkelijkePremie = waPremieInput / ((100 - huidigeKortingPercentage) / 100);
+		let nieuwePremie = oorspronkelijkePremie * ((100 - nieuweKortingPercentage) / 100) * 1.21;
+
+		// nieuwePremie = nieuwePremie * 1.032;
+
+		premieNieuw.innerHTML = nieuwePremie.toFixed(2);
+	}
 }
 
 buttonBevestigen.addEventListener('click', malusBerekening);
